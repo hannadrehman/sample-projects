bb5ad46abf15a68c3bd8bc7f8a2e906c38c38643
hannadrehman <hannad63@gmail.com>
2018-02-07 20:08:49 +0530

    removed useless files

--- Event Manager/buildDev.bat
+++
@@ -1,3 +0,0 @@
-echo 'Welcome to KM QNA UI  Test Build Deployy (development)'
-cd QnA-UI
-(npm run setlocalregistry)&&(npm run installmodules)&&(npm run deploy:build-dev)
--- Event Manager/buildStage.bat
+++
@@ -1,3 +0,0 @@
-echo 'Welcome to KM QNA UI Test Deploy  Build (Stage/production)'
-cd QnA-UI
-(npm run setlocalregistry)&&(npm run installmodules)&&(npm run deploy:build-prod)
